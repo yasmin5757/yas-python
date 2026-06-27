9e7fd35a41ed8de1961b74a7f6239adca502a7e8
yasmin5757 <IT@tmtecoman.com>
2026-06-27 17:10:37 +0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,73 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+print(3+8)
+11
+a=10
+print(a)
+10
+b=20
+print(b)
+20
+x=40
+print(x)
+40
+z=50
+print(z)
+50
+a3=80
+print(a3)
+80
+_=100
+print(_)
+100
+a=4;b=9
+print(a+b)
+13
+a,b=3,4
+print(a+b)
+7
+a=b=c=10
+print(a,b,c)
+10 10 10
+a=2,3,4
+print(a)
+(2, 3, 4)
+>>> a,b,c=(2,3,4)
+>>> print(a,b,c)
+2 3 4
+>>> first_name="yasmin"
+>>> print(first_name)
+yasmin
+>>> firstname="yasmin"
+>>> print(firstname)
+yasmin
+>>> fname="yasmin"
+>>> lname="shaik"
+>>> print(fname+lname)
+yasminshaik
+>>> print(fname+" "+lname)
+yasmin shaik
+>>> print(fname,lname)
+yasmin shaik
+>>> name="yasmin"
+>>> print(name)
+yasmin
+>>> city="muscat"
+>>> print(city)
+muscat
+>>> a=5
+>>> print(a)
+5
+>>> del a
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#40>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a3'?
+>>> name="yasmin"
+>>> print(name)
+yasmin
+>>> NAME="yasmin"
+>>> print(name)
+yasmin
